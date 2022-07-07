282d69065896da96dc56fc401bb0c52391edd905
Jeffrey Martinez <martinezjf2@gmail.com>
2022-07-07 18:16:39 -0400

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title):
+        self.title = title
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+        pass    
     pass
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    
+    def __init__(self, brand):
+        self.brand = brand
+        self.color = ''
+        self.size = 0
+        self.material = ''
+        self.condition = 'Used'
+        
+    
+    def set_color(self, color):
+        self.color = color
+        
+    def get_color(self):
+        return self.color
+    
+    def set_size(self, size):
+        self.size = size
+        
+    def set_material(self, material):
+        self.material = material
+        
+    def set_condition(self, condition):
+        self.condition = condition
+    
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
     pass
